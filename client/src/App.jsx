546cc8cb46ca1fbcc8df36646cdd256b9498fdea
Janet Jiang <janetjiang1103@gmail.com>
2023-09-05 15:11:17 -0700

    Creating RadioForm components to get user input on difficulty

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,40 @@
 import { useState,useEffect,useRef } from 'react'
 
 import './App.css'
+import RadioForm from './components/RadioForm';
 
 function App() {
-  
+  const [DifficultyLevel, setDifficultyLevely] = useState('');
+  const [result,setResult]=useState(null);
+
+  const loadQuestion = (DifficultyLevel) => {
+    const params = new URLSearchParams({ difficulty: DifficultyLevel });
+    //console.log(params);
+    fetch(`https://opentdb.com/api.php?amount=4&category=9&${params}`)
+      .then((response) => response.json())
+      .then((result) => {
+        
+        setDifficultyLevely(DifficultyLevel);
+        console.log(result);
+        setResult(result);
+    
+      });
+  }
+  useEffect(() => {
+    console.log("Updated result:", result);
+  }, [result]);
+
+  const handleSubmit = (DifficultyLevel) =>{
+    console.log(DifficultyLevel);
+    loadQuestion(DifficultyLevel);
+   }
 
   return (
     <>
       <div>
-        
-      </div>
+        <h1>Hi</h1>
+      <RadioForm handleSubmit={handleSubmit}/>
+  </div>
       
     </>
   )
---
+++ client/src/components/RadioForm.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+
+function RadioForm(props) {
+  // State to hold the selected difficulty level
+  const [selectedDifficulty, setSelectedDifficulty] = useState('');
+
+  // Handle changes when a radio button is selected
+  const handleRadioChange = (event) => {
+    setSelectedDifficulty(event.target.value);
+  };
+
+  // Handle form submission
+  const handleClick = (event) => {
+    event.preventDefault();
+    // Do something with the selected difficulty level (e.g., send it to an API)
+    console.log('Selected difficulty:', selectedDifficulty);
+    props.handleSubmit(selectedDifficulty)
+  };
+
+  return (
+    <div>
+      <h1>Quiz App</h1>
+      <p>Please select a difficulty level</p>
+      <form onSubmit={handleClick}>
+        <label>
+          <input
+            type="radio"
+            name="difficulty"
+            value="easy"
+            checked={selectedDifficulty === 'easy'}
+            onChange={handleRadioChange}
+          />
+          Easy
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="difficulty"
+            value="medium"
+            checked={selectedDifficulty === 'medium'}
+            onChange={handleRadioChange}
+          />
+          Medium
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="difficulty"
+            value="hard"
+            checked={selectedDifficulty === 'hard'}
+            onChange={handleRadioChange}
+          />
+          Hard
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default RadioForm;
